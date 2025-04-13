78a9ee100e2656315d1e48cd3b659e4fec68b5b2
andimuhriffal <andimuhriffal21@gmail.com>
2025-04-13 11:32:43 +0700

    Update Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
             initial="hidden"
             animate="visible" 
             className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
-                Backend Dev & IT Support</motion.span>
+                DevOPS & IT Support Enthusiast</motion.span>
 
                 <motion.p 
                 variants={container(1)}
